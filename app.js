639be202cf43210333db2441f8b9dbe4fe8d7c0a
Carlos <carlos811009@gmail.com>
2021-05-29 15:29:57 +0800

    add login/register page

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,10 @@ const express = require('express')
 const router = express.Router()
 const home = require('./modules/home')
 const restaurants = require('./modules/restaurants')
+const user = require('./modules/user')
 
 router.use('/', home)
 router.use('/restaurants', restaurants)
+router.use('/restaurants', user)
 
 module.exports = router
